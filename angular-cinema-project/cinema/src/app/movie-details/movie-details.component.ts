e327ec8e9f71f6f4323cef37c2a5467041538d52
djidji3 <djidji@freemail.hu>
2021-05-10 20:42:44 +0200

    all done

--- angular-cinema-project/cinema/src/app/movie-details/movie-details.component.ts
+++ angular-cinema-project/cinema/src/app/movie-details/movie-details.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsComponent implements OnInit {
 
-   @Input() chosenMovieCategory : {};
+   @Input() chosenMovieCategory ;
 
   constructor() { }
 
